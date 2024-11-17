83fa807f2a02ab381807d108a830bd36cab0fa5e
Your Name <you@example.com>
2024-11-17 20:26:50 +0530

    Updating Jenkins file for using Shared Lib

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
-import (
-	"gitlab.com/opstree/ot-go-webapp/webapp"
-)
+import "testing"
 
-func main() {
-	webapp.Run()
+func Test_main(t *testing.T) {
+	tests := []struct {
+		name string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			main()
+		})
+	}
 }
